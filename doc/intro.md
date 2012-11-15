2b7c2c9febb4370711f7db613bd9a855c5f75955
Josh Ballanco <jballanc@gmail.com>
2012-11-15 18:35:18 +0200

    Empty Immutant project template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tic-tac-doom
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/tic_tac_doom/core_test.clj
@@ -0,0 +1,7 @@
+(ns tic-tac-doom.core-test
+  (:use clojure.test
+        tic-tac-doom.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
