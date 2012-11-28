3ced573542b37efe34109e6241a1ca9bc5005556
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2012-11-28 11:14:41 -0500

    Added my my version of the solution.

--- src/tic_tac_doom/core.clj
+++ src/tic_tac_doom/core.clj
@@ -1,6 +1,22 @@
 (ns tic-tac-doom.core)
 
+(defn check-solution [elements]
+  (if (= (first elements) :e)
+    nil
+    (if (apply = elements) (first elements) nil)))
+
+(defn columns [elements]
+    (apply interleave elements))
+
+(defn diagonals [elements]
+  [(for [i (range (count elements))] [(nth (nth elements i) i)])
+   (for [i (range (count elements))] [(nth (reverse (nth elements i)) i)])])
+
+(defn possible-solutions [board]
+  (let [size (count (first board))]
+    (partition size (flatten (concat board (columns board) (diagonals board))))))
+
 (defn analyze
-  "I don't do a whole lot."
+  "Expects a game board represented as an seq of seq (rows). e.g. [[:x :y] [:x :y]]"
   [board]
-  [])
+    (some check-solution (possible-solutions board)))
